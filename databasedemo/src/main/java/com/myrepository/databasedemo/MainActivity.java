67b858bb4867a4a2a45e350ef104f84755e422dc
raoxuting <755284974@qq.com>
2017-07-09 21:50:31 +0800

    数据库增删改查功能完成

--- databasedemo/src/main/java/com/myrepository/databasedemo/MainActivity.java
+++ databasedemo/src/main/java/com/myrepository/databasedemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.myrepository.databasedemo;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +19,14 @@ public class MainActivity extends AppCompatActivity {
     Button createDb;
     @BindView(R.id.add_data)
     Button addData;
+    @BindView(R.id.delete_data)
+    Button deleteData;
+    @BindView(R.id.update_data)
+    Button updateData;
+    @BindView(R.id.query_data)
+    Button queryData;
     private DatabaseHelper helper;
+    private SQLiteDatabase bookstoreDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +34,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         helper = new DatabaseHelper(this, "BookStore.db", null, 2);
+//        bookstoreDB = helper.getWritableDatabase();
     }
 
-    @OnClick({R.id.create_db, R.id.add_data})
+    @OnClick({R.id.create_db, R.id.add_data,R.id.delete_data, R.id.update_data, R.id.query_data})
     public void onViewClicked(View view) {
+        bookstoreDB = helper.getWritableDatabase();
         switch (view.getId()) {
             case R.id.create_db:
-                helper.getReadableDatabase();
                 break;
             case R.id.add_data:
                 insertData();
                 break;
+            case R.id.delete_data:
+                deleteData();
+                break;
+            case R.id.update_data:
+                updateData();
+                break;
+            case R.id.query_data:
+                queryData();
+                break;
         }
+        bookstoreDB.close();
     }
 
     private void insertData() {
-        SQLiteDatabase bookstoreDB = helper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put("name", "鲁冰逊漂流记");
         values.put("price", 99.8);
@@ -52,8 +71,61 @@ public class MainActivity extends AppCompatActivity {
         bookstoreDB.insert("book", null, values);
 
         values.clear();
+        values.put("name", "三国演义");
+        values.put("price", 311.22);
+        bookstoreDB.insert("book", null, values);
+
+        values.clear();
+        values.put("name", "三国演义");
+        values.put("price", 188.88);
+        bookstoreDB.insert("book", null, values);
+
+        values.clear();
         values.put("name", "红楼梦");
         values.put("price", 400.98);
         bookstoreDB.insert("book", null, values);
+
+        values.clear();
+        values.put("name", "水浒传");
+        values.put("price", 366);
+        bookstoreDB.insert("book", null, values);
+
+        values.clear();
+        values.put("name", "资治通鉴");
+        values.put("price", 666);
+        bookstoreDB.insert("book", null, values);values.clear();
+
+        values.clear();
+        values.put("name", "资治通鉴");
+        values.put("price", 888);
+        bookstoreDB.insert("book", null, values);
+
+    }
+
+    private void deleteData() {
+        bookstoreDB.delete("book", null, null);
+    }
+
+    private void updateData() {
+        ContentValues values = new ContentValues();
+        values.put("price", 888);
+        bookstoreDB.update("book", values, "name like ?", new String[]{"红楼梦"});
+    }
+
+    private void queryData() {
+        Cursor cusor = bookstoreDB.query("book", null, null,null,
+                "name", "count(name) < 2", "name asc");
+        if (cusor.moveToFirst()) {
+            do {
+                int id = cusor.getInt(cusor.getColumnIndex("id"));
+                String name = cusor.getString(cusor.getColumnIndex("name"));
+                double price = cusor.getDouble(cusor.getColumnIndex("price"));
+//                Log.e("书名和价格", name + "******" + price);
+//                Log.e("价格", price + "");
+                Log.e("数据内容", id + "****" + name + "****" + price);
+            } while (cusor.moveToNext());
+
+        }
+        cusor.close();
     }
 }
