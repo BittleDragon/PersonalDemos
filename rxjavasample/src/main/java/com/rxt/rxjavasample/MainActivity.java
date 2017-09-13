bdc72f29ad12f145940055b986887dc5e1fa8fdf
BittleDragon <755284974@qq.com>
2017-09-13 17:25:45 +0800

    rxjava第一阶段学习完成

--- rxjavasample/src/main/java/com/rxt/rxjavasample/MainActivity.java
+++ rxjavasample/src/main/java/com/rxt/rxjavasample/MainActivity.java
@@ -6,10 +6,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.FlowableEmitter;
+import io.reactivex.FlowableOnSubscribe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -18,6 +25,7 @@ import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
@@ -32,8 +40,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button primary = (Button) findViewById(R.id.btn_primary);
         Button mapOperation = (Button) findViewById(R.id.btn_map);
+        Button zipbtn = (Button) findViewById(R.id.btn_zip);
+        Button sampleFilter = (Button) findViewById(R.id.btn_sample_filter);
+        Button flowable = (Button) findViewById(R.id.btn_flowable);
         primary.setOnClickListener(this);
         mapOperation.setOnClickListener(this);
+        zipbtn.setOnClickListener(this);
+        sampleFilter.setOnClickListener(this);
+        flowable.setOnClickListener(this);
 
     }
 
@@ -46,11 +60,148 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_map:
                 mapTest(2);
                 break;
-            default :
-
+            case R.id.btn_zip:
+                zipTest();
+                break;
+            case R.id.btn_sample_filter:
+                sampleOrFilter();
+                break;
+            case R.id.btn_flowable:
+                flowableTest();
                 break;
         }
 
+    }
+
+    private void flowableTest() {
+        Flowable.create(new FlowableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull FlowableEmitter<Integer> e) throws Exception {
+
+            }
+        }, BackpressureStrategy.ERROR)//BackpressureStrategy.BUFFER:存储事件的容器无限大，类似于observable，有可能OOM
+                //BackpressureStrategy.DROP:超出下游处理能力的事件将被丢弃，不会存入容器造成OOM
+                //BackpressureStrategy.LATEST:容器中保存最新的s.request(n)个数的事件
+                .subscribe(new Subscriber<Integer>() {
+
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        s.request(4);
+                    }
+
+                    @Override
+                    public void onNext(Integer integer) {
+                        Log.e(TAG, "onNext: " + integer);
+                    }
+
+                    @Override
+                    public void onError(Throwable t) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete: ");
+                    }
+                });
+    }
+
+    private void sampleOrFilter() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                for (int i = 1; ; i++) {
+                    e.onNext(i);
+                }
+            }
+        }).subscribeOn(Schedulers.io())
+//                .filter(new Predicate<Integer>() {
+//                    @Override
+//                    public boolean test(@NonNull Integer integer) throws Exception {
+//                        return integer % 20 == 0;
+//                    }
+//                })
+                .sample(2, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Integer>() {
+                    @Override
+                    public void accept(@NonNull Integer integer) throws Exception {
+                        Log.e(TAG, "accept: " + integer);
+                    }
+                });
+    }
+
+    private void zipTest() {
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
+                Log.e(TAG, "et: 1");
+                e.onNext(1);
+                Thread.sleep(1000);
+
+                Log.e(TAG, "et: 2");
+                e.onNext(2);
+                Thread.sleep(1000);
+
+                Log.e(TAG, "et: 3");
+                e.onNext(3);
+                Thread.sleep(1000);
+
+                Log.e(TAG, "et: 4");
+                e.onNext(4);
+//                Thread.sleep(1000);
+
+                Log.e(TAG, "et onComplete1");
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.io());
+        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                Log.e(TAG, "et: a");
+                e.onNext("a");
+                Thread.sleep(1000);
+
+                Log.e(TAG, "et: b");
+                e.onNext("b");
+                Thread.sleep(1000);
+
+                Log.e(TAG, "et: c");
+                e.onNext("c");
+                Thread.sleep(1000);
+
+                Log.e(TAG, "et onComplete2");
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.io());
+
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+            @Override
+            public String apply(@NonNull Integer integer, @NonNull String s) throws Exception {
+                return integer + s;
+            }
+        }).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull String s) {
+                        Log.e(TAG, "onNext: " + s);
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e(TAG, "onComplete");
+                    }
+                });
     }
 
     private void mapTest(int flag) {
@@ -79,6 +230,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         });
                 break;
             case 2:
+                //flatMap无序发送，concatMap为严格按顺序发送
                 Observable.create(new ObservableOnSubscribe<Integer>() {
                     @Override
                     public void subscribe(@NonNull ObservableEmitter<Integer> e) throws Exception {
@@ -91,9 +243,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public ObservableSource<String> apply(@NonNull Integer integer) throws Exception {
                         List<String> list = new ArrayList<>();
-//                        for (int i = 1; i <= 4; i++) {
-//                            list.add("value" + i);
-//                        }
                         list.add("a");
                         list.add("b");
                         list.add("c");
