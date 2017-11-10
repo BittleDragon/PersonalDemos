9123c10f9f5888477b1557ebe9fe036feea7a5bc
BittleDragon <755284974@qq.com>
2017-11-10 14:33:12 +0800

    隐式意图测试demo结束

--- implicitintent/src/main/java/com/rxt/implicitintent/MainActivity.java
+++ implicitintent/src/main/java/com/rxt/implicitintent/MainActivity.java
@@ -6,10 +6,15 @@ import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,18 +26,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent("com.rxt.action.1");
-                intent.setDataAndType(Uri.parse("file://abc"), "image/jpg");
+                File file = new File(Environment
+                        .getExternalStorageDirectory().getAbsolutePath() + "/abc.jpg");
+                Uri fileUri = FileProvider.getUriForFile(MainActivity.this, BuildConfig.
+                        APPLICATION_ID + ".fileProvider", file);
+                intent.setDataAndType(fileUri, "image/jpg");
                 ResolveInfo resolveInfo = getPackageManager()
                         .resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 ComponentName componentName = intent.resolveActivity(getPackageManager());
-//                if (componentName == null) {
-//
-//                }
+                if (componentName != null) {
+                    Log.e("组件名", componentName.getShortClassName());//".SecondActivity"
+                }
                 if (resolveInfo == null) {
                     Toast.makeText(MainActivity.this, "没有匹配的activity", Toast.LENGTH_SHORT).show();
                 } else {
-                    Log.e("activity名字", resolveInfo.activityInfo.targetActivity);
-//                    startActivity(intent);
+                    String activityName = resolveInfo.activityInfo.name;//"com.rxt.implicitintent.SecondActivity"
+                    String pureName = activityName.substring(activityName.lastIndexOf(".") + 1);
+                    Log.e("activity名字", pureName);
+                    if (TextUtils.equals(pureName, "SecondActivity"))
+                        startActivity(intent);
+                    else {
+                        Log.e("activity", "不匹配");
+                    }
                 }
             }
         });
